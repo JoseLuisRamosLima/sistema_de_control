4654790477c6ff43695381b267b153fa5099fa84
jose ramos <jramos@fcpn.edu.bo>
2024-07-22 19:00:15 -0400

    mod 3 conexion afuera

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,11 +3,13 @@ import axios from "axios";
 
 const instance = axios.create({
     // **************************************
-    // CONSUMO DE ENDPOINT AL CODIGO LOCAL DE BACKEND 
+    // CONSUMO DE ENDPOINT AL CODIGO LOCAL DE BACKEND
     // baseURL: 'http://localhost:3000/api',
     // **************************************
-    // CONSUMO DE ENSPOINT AL CODIGO DE PRODUCCION HOST ====RENDER====
+    // CONSUMO DE ENSPOINT AL CODIGO DE PRODUCCION HOST ====RENDER
     baseURL: 'https://sistema-de-control.onrender.com/api',
+    
+
     withCredentials:true
 })
 
