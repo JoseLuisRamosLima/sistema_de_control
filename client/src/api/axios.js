9430ae5157ed9a19f61b84989e6f881d66f785a0
jose ramos <jramos@fcpn.edu.bo>
2024-07-25 14:34:57 -0400

    mod5 jueves

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -19,19 +19,21 @@
 
 import axios from "axios";
 
-// const token = localStorage.getItem('token');
-
 const instance = axios.create({
-    // **************************************
-    // CONSUMO DE ENDPOINT AL CODIGO LOCAL DE BACKEND
-    // baseURL: 'http://localhost:3000/api',
-    // **************************************
-    // CONSUMO DE ENSPOINT AL CODIGO DE PRODUCCION HOST ====RENDER
     baseURL: 'https://sistema-de-control.onrender.com/api',
     withCredentials: true,
-    // headers: {
-    //     'Authorization': `Bearer ${token}`
-    // }
 })
+
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+
 
 export default instance
