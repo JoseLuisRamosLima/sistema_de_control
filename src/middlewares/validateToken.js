743a9e66cedffd157178259b18420ec7697f4ff3
jose ramos <jramos@fcpn.edu.bo>
2024-07-25 16:13:35 -0400

    mod11jueves

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,14 +4,21 @@ import {TOKEN_SECRET} from "../config.js";
 
 
 export const authRequired = (req, res, next)=>{
-    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    // const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
     // const { token } = req.cookies;
+    const authHeader = req.headers['authorization'];
 
-    if(!token){
+    if(!authHeader){
         return res.status(401).json({
             message: "No token, authorizacion denegado"
         });
     } 
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({
+            message: "Token malformado, authorization denied"
+        });
+    }
     jwt.verify(token, TOKEN_SECRET, (err, user)=>{
         if(err) {
             console.error("Error al verificar token: ", err);
